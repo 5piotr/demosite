e5733de7935e4698a1899999aeaadde4f03ba067
Piotr Pietka <piotr-pietka@wp.pl>
2021-01-18 17:11:44 +0100

    added apartment form

--- demosite/static/demoapp1/js/apartment_price_estimator.js
+++ demosite/static/demoapp1/js/apartment_price_estimator.js
@@ -23,3 +23,41 @@ function onMapClick(e) {
 }
 
 mymap.on('click', onMapClick);
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields and adding spinner
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+
+      }, false);
+
+    });
+
+  }, false);
+
+})();
+
+// spinner
+$('#form').on('change', function() {
+  $(".btn").click(function() {
+    // disable button
+    // $(this).prop("disabled", true);
+    // add spinner to button
+    $('#spinner').html(
+      `<div class="d-flex align-items-center">
+        <strong>Loading...</strong>
+        <div class="spinner-border ml-auto" role="status" aria-hidden="true"></div>
+      </div>`
+    );
+  });
+});
--- demosite/static/demoapp1/js/simple_gesture_recognition.js
+++ demosite/static/demoapp1/js/simple_gesture_recognition.js
@@ -1,7 +1,28 @@
-// putting only file name in the selection box
-$('#inputGroupFile02').on('change',function(){
-    //get the file name
-    var fileName = $(this).val().replace('C:\\fakepath\\', " ");
-    //replace the "Choose a file" label
-    $(this).next('.custom-file-label').html(fileName);
-})
+// file size validation, path writing, spinner
+$('#validatedCustomFile').on('change', function() {
+
+    const size =
+       (this.files[0].size / 1024 / 1024).toFixed(2);
+
+    if (size > 4) {
+        alert("File must be smaller than 4 MB");
+        $(this).val('');
+    } else {
+      //get the file name
+      var fileName = $(this).val().replace('C:\\fakepath\\', " ");
+      //replace the "Choose a file" label
+      $(this).next('.custom-file-label').html(fileName);
+      // spinner
+      $(".btn").click(function() {
+        // disable button
+        // $(this).prop("disabled", true);
+        // add spinner to button
+        $('#spinner').html(
+          `<div class="d-flex align-items-center">
+            <strong>Loading...</strong>
+            <div class="spinner-border ml-auto" role="status" aria-hidden="true"></div>
+          </div>`
+        );
+      });
+    }
+});
