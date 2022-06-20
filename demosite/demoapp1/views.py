898371f36fb515f79c6853981f002f48da34f275
Piotr Pietka <piotr-pietka@wp.pl>
2022-06-20 23:43:06 +0200

    apartment estimator release for 2022q2

--- demosite/demoapp1/views.py
+++ demosite/demoapp1/views.py
@@ -24,48 +24,48 @@ class IndexView(TemplateView):
 class AboutView(TemplateView):
     template_name = 'demoapp1/about.html'
 
-def calculator(request):
-    try:
-        hue = request.POST['query']
-        ret = eval(hue)
-        dict = {
-            'hue' : hue,
-            'ret' : ret,
-        }
-        return render(request,'demoapp1/calculator.html', context=dict)
-    except (NameError, ZeroDivisionError, ValueError, TypeError):
-        return render(request,'demoapp1/calculator.html', context={'ret' : 'Error'})
-    except:
-        return render(request,'demoapp1/calculator.html')
-
-def graphing_calculator(request):
-    try:
-        eq = request.POST['equation']
-        x_min = float(request.POST['x_min'])
-        x_max = float(request.POST['x_max'])
-        # step = float(request.GET['step'])
-        # x_data = numpy.arange(x_min,x_max,step)
-        x_data = numpy.linspace(x_min,x_max,200,True)
-        y_data = [eval(eq) for x in x_data]
-        fig = go.Figure()
-        fig.add_trace(go.Scatter(x=x_data,y=y_data,mode='lines'))
-        fig.update_layout(margin=dict(l=10, r=10, b=10, t=10))
-        fig.update_layout(
-                title={'text': 'f(x) = ' + eq,
-                        'y':0.9,
-                        'x':0.5,
-                        'xanchor': 'center',
-                        'yanchor': 'top'},
-                xaxis_title="x",
-                yaxis_title="f(x)",
-                paper_bgcolor="#caf0f8",
-                plot_bgcolor='#d8f3dc')
-        plot_div = plot(fig, output_type='div')
-        return render(request, 'demoapp1/graphing_calculator.html', context={'plot_div': plot_div})
-    except (NameError, ZeroDivisionError, ValueError, TypeError, SyntaxError, AttributeError):
-        return render(request,'demoapp1/graphing_calculator.html', context={'plot_div' : 'Error'})
-    except:
-        return render(request,'demoapp1/graphing_calculator.html')
+# def calculator(request):
+#     try:
+#         hue = request.POST['query']
+#         ret = eval(hue)
+#         dict = {
+#             'hue' : hue,
+#             'ret' : ret,
+#         }
+#         return render(request,'demoapp1/calculator.html', context=dict)
+#     except (NameError, ZeroDivisionError, ValueError, TypeError):
+#         return render(request,'demoapp1/calculator.html', context={'ret' : 'Error'})
+#     except:
+#         return render(request,'demoapp1/calculator.html')
+#
+# def graphing_calculator(request):
+#     try:
+#         eq = request.POST['equation']
+#         x_min = float(request.POST['x_min'])
+#         x_max = float(request.POST['x_max'])
+#         # step = float(request.GET['step'])
+#         # x_data = numpy.arange(x_min,x_max,step)
+#         x_data = numpy.linspace(x_min,x_max,200,True)
+#         y_data = [eval(eq) for x in x_data]
+#         fig = go.Figure()
+#         fig.add_trace(go.Scatter(x=x_data,y=y_data,mode='lines'))
+#         fig.update_layout(margin=dict(l=10, r=10, b=10, t=10))
+#         fig.update_layout(
+#                 title={'text': 'f(x) = ' + eq,
+#                         'y':0.9,
+#                         'x':0.5,
+#                         'xanchor': 'center',
+#                         'yanchor': 'top'},
+#                 xaxis_title="x",
+#                 yaxis_title="f(x)",
+#                 paper_bgcolor="#caf0f8",
+#                 plot_bgcolor='#d8f3dc')
+#         plot_div = plot(fig, output_type='div')
+#         return render(request, 'demoapp1/graphing_calculator.html', context={'plot_div': plot_div})
+#     except (NameError, ZeroDivisionError, ValueError, TypeError, SyntaxError, AttributeError):
+#         return render(request,'demoapp1/graphing_calculator.html', context={'plot_div' : 'Error'})
+#     except:
+#         return render(request,'demoapp1/graphing_calculator.html')
 
 def simple_gesture_recognition(request):
     try:
@@ -111,60 +111,47 @@ def apartment_price_estimator(request):
             raise ValueError('Please select location in Poland')
 
         if request.POST['market'] == 'primary':
-            market = 'pierwotny'
+            market = 'primary_market'
         elif request.POST['market'] == 'aftermarket':
-            market = 'wtorny'
+            market = 'aftermarket'
         built = float(request.POST['built'])
         area = float(request.POST['area'])
-        if request.POST['rooms'] == '> 8':
-            rooms = 'więcej niż 8'
-        else:
-            rooms = request.POST['rooms']
-        if request.POST['floor'] == '0':
-            floor = 'parter'
-        elif request.POST['floor'] == '30':
-            floor = 'powyżej 30'
-        else:
-            floor = request.POST['floor']
-        if request.POST['floors'] == '0':
-            floors = '0 (parter)'
-        elif request.POST['floors'] == '30':
-            floors = 'powyżej 30'
-        else:
-            floors = request.POST['floors']
+        rooms = request.POST['rooms']
+        floor = request.POST['floor']
+        floors = request.POST['floors']
         if int(request.POST['floor']) > int(request.POST['floors']):
             raise ValueError('Selected floor should be in complience with the total number of floors')
 
         # cluster assignment
-        kmeans = joblib.load(os.path.join(os.path.dirname(os.path.abspath(__file__)),'500_means_cls_2021q2'))
+        kmeans = joblib.load(os.path.join(os.path.dirname(os.path.abspath(__file__)),'k_means_model'))
         cluster = kmeans.predict([[lat,lng]])[0]
 
         # preparint input for estimation
-        infile = open(os.path.join(os.path.dirname(os.path.abspath(__file__)),'dummy_apartment_frame_2021q2'),'rb')
+        infile = open(os.path.join(os.path.dirname(os.path.abspath(__file__)),'dummy_apartment_frame'),'rb')
         dummy_frame = pkl.load(infile)
         infile.close()
 
         dummy_frame['area'] = area
         dummy_frame['build_yr'] = built
-        if market == 'wtorny':
-            dummy_frame.market_wtorny = 1
+        if market == 'primary_market':
+            dummy_frame.market_primary_market = 1
         if rooms != '1':
             dummy_frame['rooms_' + str(rooms)] = 1
-        if floor != '1':
+        if floor != '0':
             dummy_frame['floor_' + str(floor)] = 1
-        if floors != '0 (parter)':
+        if floors != '0':
             dummy_frame['floors_' + str(floors)] = 1
         if cluster != 0:
             dummy_frame['cluster_' + str(cluster)] = 1
 
         # ann estimation
-        model_ann = load_model(os.path.join(os.path.dirname(os.path.abspath(__file__)),'2021q2_500_2021-07-13--17-29'))
-        scaler_ann = joblib.load(os.path.join(os.path.dirname(os.path.abspath(__file__)),'scaler_500_2021q2'))
+        model_ann = load_model(os.path.join(os.path.dirname(os.path.abspath(__file__)),'ann_model_2022-06-19--11-31'))
+        scaler_ann = joblib.load(os.path.join(os.path.dirname(os.path.abspath(__file__)),'scaler'))
         pred_ann = model_ann.predict(scaler_ann.transform(dummy_frame))[0][0]
         pred_ann = int(pred_ann)
 
         # random forest estimation
-        model_rf = joblib.load(os.path.join(os.path.dirname(os.path.abspath(__file__)),'random_forest_model_2021q2'))
+        model_rf = joblib.load(os.path.join(os.path.dirname(os.path.abspath(__file__)),'random_forest_model'))
         pred_rf = model_rf.predict(dummy_frame)[0]
         pred_rf = int(pred_rf)
 
@@ -194,8 +181,8 @@ def apartment_price_estimator(request):
                         'built':int(built),
                         'area':int(area),
                         'rooms':rooms,
-                        'floor':request.POST['floor'],
-                        'floors':request.POST['floors'],
+                        'floor':floor,
+                        'floors':floors,
                         'pred_ann':pred_ann,
                         'pred_rf':pred_rf,
                         'pred_ann_m':int(pred_ann_m),
@@ -216,4 +203,3 @@ def privacy_policy(request):
 
 def under_construction(request):
     return render(request,'demoapp1/under_construction.html')
-    
